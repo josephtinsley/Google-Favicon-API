8b5037a498ff8e0edb400273c46d525bbde0af2b
Joseph Tinsley <joseph.tinsley@gmail.com>
2011-03-01 02:15:33 -0500

    Small changes basically to test push function

--- Google-Favicon.php
+++ Google-Favicon.php
@@ -1,17 +1,17 @@
 <?PHP
-function getFavicon($var)
-{
-$filName=substr($var,0,-4);
-$fp = fopen ('folder/'.$filName.'.png', 'w+');
-$ch = curl_init('http://www.google.com/s2/favicons?domain='.$var);
-curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_exec($ch);
-curl_close($ch);
-fclose($fp);
-}
+  function getFavicon($var)
+  {
+    $filName = substr($var,0,-4);
+    $fp = fopen ('folder/'.$filName.'.png', 'w+');
+    $ch = curl_init('http://www.google.com/s2/favicons?domain='.$var);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_FILE, $fp);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_exec($ch);
+    curl_close($ch);
+    fclose($fp);
+  }
 
-$var='techcrunch.com';
-getFavicon($var);
+  $var =  'techcrunch.com';
+  getFavicon($var);
 ?>
